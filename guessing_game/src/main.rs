f3203bca800c6ea9c010aa54c281ef6838fe3439
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-03 17:18:42 +0200

    stuff

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,16 +1,30 @@
-use std::io;
+use std::io; // input/output standar library
+use rand::Rng; // random number generator crate
+use std::cmp::Ordering; // Stuff
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guess the number!"); // prints the string to the screen
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101); // it generates a randon number between 1 and 100
+
+    println!("The secret number is {}", secret_number);
+
+    println!("Please input your guess."); // prints the string to the screen
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
-    
-    println!("You guessed: {}", guess)
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    loop {
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+    }
 }

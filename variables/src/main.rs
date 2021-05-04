99e3416399ea0b18151ce62cf1be0e45623d9917
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-04 19:14:09 +0200

    array

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,21 @@
+fn array_elements(){
+    use std::io;
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index");
+
+    let mut index = String::new();
+
+    io::stdin().read_line(&mut index).expect("Failed to read line");
+
+    let index: usize = index.trim().parse().expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {} is: {}", index, element);
+}
+
 fn main() {
   /*   let mut x = 5;
     println!("The value of x is: {}", x);
@@ -51,4 +69,6 @@ fn main() {
     let one = x.2;
 
     println!("{},{},{}", one, six_point_four, five_hundred);
+
+    array_elements();
 }

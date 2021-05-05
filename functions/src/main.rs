32e7c87b8f0214bb33e25dd347e63facbf19fe22
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-05 15:19:44 +0200

    functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,24 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    another_function(5);
+    let x = five();
+
+    let _x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+
+    };
+
+    println!("the value of y is: {}", y);
+    println!("The value of x is {}", x);
 }
 
-fn another_function(){
-    println!("another function")
+fn another_function(x: i32){
+    println!("The value of x is: {}", x);
+}
+
+fn five() -> i32 {
+    5
 }

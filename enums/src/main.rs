eb4ba53669f2bd95c2de3d8133d07633a0eb810c
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-06 17:02:48 +0200

    enums

---
+++ enums/src/main.rs
@@ -0,0 +1,25 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+impl Coin {
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter => 25,
+        }
+    }
+}
+
+
+fn main() {
+    let coin = Coin::value_in_cents(Coin::Dime);
+
+    println!("{}", coin);
+}
+

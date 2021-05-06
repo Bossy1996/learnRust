4bb6ce3a659901d97b24973f8da56d2614e1e75d
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-06 16:15:57 +0200

    struct_example

--- struct_example/src/main.rs
+++ struct_example/src/main.rs
@@ -1,12 +1,49 @@
-fn main() {
-   let rect1 = (30, 50);
-
-   println!(
-       "The area of the rectangle is {} square pixels.",
-       area(rect1)
-   );
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    heigth: u32,
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.heigth
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.heigth > other.heigth
+    }
+    
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            heigth: size
+        }
+    }
+}
+
+fn main() {
+   let rect1 = Rectangle {
+       width: 30,
+       heigth: 50,
+   };
+
+   let rect2 = Rectangle {
+       width: 10,
+       heigth: 40,
+   };
+
+   let rect3 = Rectangle {
+       width: 60,
+       heigth: 45,
+   };
+
+   let sq = Rectangle::square(3);
+
+   println!("Can rec1 hold rect2? {}", rect1.can_hold(&rect2));
+   println!("Can rec1 hold rect3? {}", rect1.can_hold(&rect3));
+
+   println!(
+       "The area of the rectangle is {:#?} square pixels.",
+       rect1.area()
+   );
 }

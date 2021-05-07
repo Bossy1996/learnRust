ba9900e0218ac47d07dda1db69ee4077fd10bd17
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-07 15:42:17 +0200

    stuff

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -24,8 +24,36 @@ fn value_in_cents(coin: Coin) -> u8 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
 
 fn main() {
-    println!("Hello world");
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    let another_some_value_u8 = Some(0u8);
+    if let Some(3) = another_some_value_u8 {
+        println!("Three");
+    }
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}", state);
+    }else {
+        count += 1;
+    }
 }
 

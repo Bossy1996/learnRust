2f6689ecd4cc34f028bee757ff8e1230c234c932
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-07 15:24:04 +0200

    new thing

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,25 +1,31 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
-impl Coin {
-    fn value_in_cents(coin: Coin) -> u8 {
-        match coin {
-            Coin::Penny => 1,
-            Coin::Nickel => 5,
-            Coin::Dime => 10,
-            Coin::Quarter => 25,
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
         }
     }
 }
 
 
 fn main() {
-    let coin = Coin::value_in_cents(Coin::Dime);
-
-    println!("{}", coin);
+    println!("Hello world");
 }
 

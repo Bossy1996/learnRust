36bdf1ad3ffce5bcdf99b0764b7276e9505fd565
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-09 20:17:18 +0200

    bringing into scope with use keyword

--- my-project/restaurant/src/lib.rs
+++ my-project/restaurant/src/lib.rs
@@ -41,6 +41,8 @@ mod back_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
@@ -54,4 +56,8 @@ pub fn eat_at_restaurant() {
 
     let order1 = back_of_house::Appetizer::Soup;
     let order2 = back_of_house::Appetizer::Salad;
+
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }

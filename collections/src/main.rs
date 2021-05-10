8ea33da40fb04f462dee0f9bcb8ae421b031b130
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-10 14:27:09 +0200

    vector with enum

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -35,4 +35,16 @@ fn main() {
     for i in &mut vec {
         *i += 50;
     }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12)
+    ];
+}
+
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
 }

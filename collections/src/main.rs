c7878e9dc392371082a427bef12120e84e6e114e
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-10 16:54:42 +0200

    hash

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -97,4 +97,25 @@ fn hashmaps() {
 
     // Overwritting a value
     // scores.insert(String::from("Blue"), 25);
+
+    //Only inserting a Value if the key has no value
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    // Updating a value based on the old value
+    let text = "Hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }

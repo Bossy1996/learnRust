b8d03258a45d48a1727baf591a667f2e56c2e4f7
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-12 23:39:57 +0200

    stuff

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -108,7 +108,7 @@ fn panic_or_not_panic() {
 
         if guess < 1 || guess > 100 {
             println!("The secret number will be between 1 and 100.");
-            continue,
+            continue;
         }
 
         match guess.cmp(&secret_number){
@@ -124,7 +124,7 @@ pub struct Guess {
 impl Guess {
     pub fn new(value: i32) -> Guess {
         if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}", value);
+            panic!("Guess value must be between 1 and 100, got {}", value);   
         }
 
         Guess {value}

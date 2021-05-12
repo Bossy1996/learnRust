dfaac26592773a1a7bb43c14aae73e812f037193
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-12 23:30:40 +0200

    stuff

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -97,7 +97,7 @@ fn panic_or_not_panic() {
     let home: IpAddr = "127.0.0.1".parse().unwrap();
 }
 
-fn creating_custom_types_for_validation() {
+/* fn creating_custom_types_for_validation() {
     loop {
         // --snip--
         
@@ -114,5 +114,23 @@ fn creating_custom_types_for_validation() {
         match guess.cmp(&secret_number){
 
         }
+    }
+} */
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess {value}
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
     }
 }

d6a965aefb702c263e4f7439118e344c006a1eae
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-13 00:04:45 +0200

    Largest function

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,5 +1,29 @@
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+
+    let number_list = vec![120, 32, 6000, 89, 54, 2, 43, 8];
 
     let mut largest = number_list[0];
 

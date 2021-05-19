665ea16c54fa323b59da7f8d4d34b19fa1a16a0a
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-19 15:21:30 +0200

    iterator sum test

--- functional_language/src/iterators.rs
+++ functional_language/src/iterators.rs
@@ -13,3 +13,14 @@ pub trait Iterator {
 
     // methods with default implementations elided
 }
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
+}

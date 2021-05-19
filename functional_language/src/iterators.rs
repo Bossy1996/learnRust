5cee161b843a7fcd905734ae1fd8e8b4761e70bf
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-19 15:30:53 +0200

    stuff

--- functional_language/src/iterators.rs
+++ functional_language/src/iterators.rs
@@ -24,3 +24,11 @@ fn iterator_sum() {
 
     assert_eq!(total, 6);
 }
+
+fn consuming_iterator() {
+    let v1 = Vec<i32> = vec![1, 2, 3];
+
+    let v2 = Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
+}

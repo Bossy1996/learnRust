4b4aafba7905e8e08463c1cc1307d7f15e0ba671
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-19 15:47:09 +0200

    sutff

--- functional_language/src/capture_env.rs
+++ functional_language/src/capture_env.rs
@@ -44,3 +44,32 @@ mod tests {
         ]);
     }
 }
+
+struct Counter {
+    count: u32
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn new(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}

713d48b4499344a7d0b9240bb7d9a34f9add9ce9
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-23 13:10:08 +0200

    tree fn

--- smartpointers/messenger/src/treestructure.rs
+++ smartpointers/messenger/src/treestructure.rs
@@ -1,8 +1,21 @@
 use std::cell::RefCell;
-use sdt::rc::Rc;
+use sdt::rc::{Rc, Weak};
 
 #[derive(Debug)]
 struct Node {
 	value: i32,
-	children: RefCell<Vec<Rc<Node>>>,
+	children: RefCell<Weak<Node>>>,
+	parent: RefCell<Vec<Rc<Node>>>,
+}
+
+fn tree() {
+	let leaf = Rc::new(Node {
+		value: 3,
+		children: RefCell::new(vec![]),
+	});
+
+	let branch = RefCell::new(Node {
+		value: 5,
+		children: RefCell::new(vec![Rc::clone(&leaf)]),
+	});
 }

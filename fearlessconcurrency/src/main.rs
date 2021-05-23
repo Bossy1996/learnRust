15efe7d2c0e69a471036462b42ac0c46b86624d2
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-23 16:38:12 +0200

    stuff

--- fearlessconcurrency/src/main.rs
+++ fearlessconcurrency/src/main.rs
@@ -38,7 +38,75 @@ fn main() {
     println!("Got: {}", received);
 
     // Channels and Ownership transference
+    // The code below won't compile because the val ownership has been transfered
+    // let (tx, rx) = mpsc::channel();
 
-    
+    // thread::spawn(move || {
+    //     let val = String::from("hi");
+    //     tx.send(val).unwrap();
+    //     println!("val is {}", val);
+    // });
 
+    // let received = rx.recv().unwrap();
+    // println!("Got: {}", received);
+
+
+    // Sending multiple values and seeing the reciever wait
+
+    let (request, response) = mpsc::channel();
+
+    thread::spawn(move || {
+    	let vals = vec![
+    		String::from("hi"),
+    		String::from("from"),
+    		String::from("the"),
+    		String::from("thread"),
+    	];
+
+    	for val in vals {
+    		request.send(val).unwrap();
+    		thread::sleep(Duration::from_secs(1));
+    	}
+    });
+
+    for received in response {
+    	println!("Got {}", received);
+    }
+
+    // Creating multiple producers by cloning the transmitter
+
+    let (request2, response2) = mpsc::channel();
+
+    let request2_1 = request2.clone();
+    thread::spawn(move || {
+    	let vals = vec![
+    		String::from("hi"),
+    		String::from("from"),
+    		String::from("the"),
+    		String::from("thread"),
+    	];
+
+    	for val in vals {
+    		request2.send(val).unwrap();
+    		thread::sleep(Duration::from_secs(1));
+    	}
+    });
+
+    thread::spawn(move || {
+    	let vals = vec![
+    		String::from("more"),
+    		String::from("messages"),
+    		String::from("for"),
+    		String::from("you"),
+    	];
+
+    	for val in vals {
+    		request2.send(val).unwrap();
+    		thread::sleep(Duration::from_secs(1));
+    	}
+    });
+
+    for received in response2 {
+    	println!("Got: {}", received);
+    }
 }

aed47974c1e65bd31790273d1c9f1694d8ab74cd
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-23 16:17:58 +0200

    Using message passing to transfer data between threads

--- fearlessconcurrency/src/main.rs
+++ fearlessconcurrency/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
     let handle = thread::spawn(|| {
@@ -15,5 +16,29 @@ fn main() {
     	println!("hi numnber {} from the main thread", i);
     	thread::sleep(Duration::from_millis(1));
     }
+
+    let v = vec![1, 2, 3];
+
+    let handle2 = thread::spawn(move || { // moves the ownership of v to de closure
+    	println!("Here's a vector: {:?}", v);
+    });
+
+    handle2.join().unwrap();
+
+    // Using message passing to transfer data between threads
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+    	let val = String::from("hi");
+    	tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+
+    // Channels and Ownership transference
+
+    
 
 }

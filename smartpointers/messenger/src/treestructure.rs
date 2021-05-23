282fd77dcd01bd42f08f2e0a79e1157e847f5caa
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-23 13:15:36 +0200

    tree

--- smartpointers/messenger/src/treestructure.rs
+++ smartpointers/messenger/src/treestructure.rs
@@ -12,10 +12,18 @@ fn tree() {
 	let leaf = Rc::new(Node {
 		value: 3,
 		children: RefCell::new(vec![]),
+		parent: RefCell::new(Weak::new()),
 	});
+
+	println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
 	let branch = RefCell::new(Node {
 		value: 5,
 		children: RefCell::new(vec![Rc::clone(&leaf)]),
+		parent: RefCell::new(Weak::new()),
 	});
+
+	*leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+	println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }

642409623a921ff74ef3f5ce7f5668eb93b79e9d
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-26 17:00:34 +0200

    macro

--- hello_macro/hello_macro_derive/src/lib.rs
+++ hello_macro/hello_macro_derive/src/lib.rs
@@ -1,7 +1,30 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quotes::queote;
+use quote::queote;
+use syn;
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation
+    impl_hello_macro(&ast);
+}
+
+fn impl_hello_macro(ast: &syn::DerviceInput) -> TokenStream {
+    let name = &ast.indent;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! my name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
 
 
 #[cfg(test)]

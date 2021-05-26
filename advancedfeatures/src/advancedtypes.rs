82843887201b9351ab9a18e1d6d3010991f2c4f8
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-05-26 15:31:56 +0200

    Advanced Types

---
+++ advancedfeatures/src/advancedtypes.rs
@@ -0,0 +1,29 @@
+type Kilometers = i32;
+
+let x: i32 = 5;
+let y: Kilometers = 5;
+
+println!("x + y = {}", x, y);
+
+type Thunk = Box<dyn Fn() + Send + 'static>;
+
+let f: Thunk = Box::new(|| println!("hi"));
+
+fn takes_long_type(f: Thunk) {
+    // --snip--
+}
+
+fn returns_long_type() -> Thunk {
+    // --snip--
+}
+
+use sdt::fmt;
+use std::io::Error;
+
+pub trait Write {
+    fn write(&mut self, buf: &[i8]) -> Result<usize, Error>;
+    fn flush(&mut self) -> Result<(), Error>;
+
+    fn write_all(&mut self, buf: &[i8]) -> Result<(), Error>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<(), Error>;
+}
